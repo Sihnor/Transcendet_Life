bc2b21ff255367c979b56aff7381feb813e6b217
Sihnor <m.rudolph.programming@outlook.com>
2023-06-12 23:34:18 +0200

    Added the BLueprint for GodHand and the functionality of the World Rotation. There is still a Bug that need to be fixed. Moved the RiderLink inside the Game

--- Source/Transcendet_Life/Characters/GodHand.cpp
+++ Source/Transcendet_Life/Characters/GodHand.cpp
@@ -1,40 +1,62 @@
 ﻿// Fill out your copyright notice in the Description page of Project Settings.
 
-
-
 #include "GodHand.h"
-#include "Camera/CameraComponent.h"
-#include <Components/StaticMeshComponent.h>
-#include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "InputTriggers.h"
+#include "Camera/CameraComponent.h"
+#include "Components/RectLightComponent.h"
+#include "Engine/StaticMeshActor.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Kismet/GameplayStatics.h"
+
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values
 AGodHand::AGodHand() {
 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
-	// Setting up the Enhanced Player Input
-	// Get Player Controller
-	APlayerController* PlayerController = Cast<APlayerController>(this->GetController());
-	if (PlayerController) {
-		UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
-		if (Subsystem) {
-			Subsystem->AddMappingContext(this->MappingContext,0);
-		}
-	}
-
 	// Setup component hierarchy
-	this->PlayerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlayerMesh"));
-	RootComponent = PlayerMesh;
-	this->PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
-	this->PlayerCamera->SetupAttachment(this->PlayerMesh);
+	this->Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootComp"));
+	SetRootComponent(this->Root);
+
+	this->PlayerMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("PlayerMeshComp"));
+	this->PlayerMesh->SetRelativeRotation(FRotator3d(0, 180, 0));
+	this->PlayerMesh->SetRelativeScale3D(FVector(0.05));
+	this->PlayerMesh->SetupAttachment(this->Root);
+
+	this->SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	this->SpringArm->SetRelativeRotation(FRotator(-30, 0, 0));
+	this->SpringArm->TargetArmLength = 150;
+	this->SpringArm->SetupAttachment(this->Root);
+
+	this->RectLightComponent = CreateDefaultSubobject<URectLightComponent>(TEXT("Light"));
+	this->RectLightComponent->SetRelativeLocation(FVector(100, 0, 120));
+	this->RectLightComponent->Intensity = 10;
+	this->RectLightComponent->SetupAttachment(this->SpringArm);
+
+	this->PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCameraComp"));
+	this->PlayerCamera->SetupAttachment(this->SpringArm);
+	this->PlayerCamera->FieldOfView = 40.0f;
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
 }
 
 // Called when the game starts or when spawned
 void AGodHand::BeginPlay() {
 	Super::BeginPlay();
-	
+
+	// Setting up the Enhanced Player Input
+	if (const APlayerController* PlayerController = Cast<APlayerController>(this->GetController())) {
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<
+			UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+			Subsystem->AddMappingContext(this->MappingContext, 0);
+		}
+	}
+
+	// Get the all Actor from 
+	this->GetRotaingWorldFormAllActors();
 }
 
 // Called every frame
@@ -42,18 +64,98 @@ void AGodHand::Tick(float DeltaTime) {
 	Super::Tick(DeltaTime);
 }
 
+void AGodHand::GetRotaingWorldFormAllActors() {
+	TArray<AActor*> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), AStaticMeshActor::StaticClass(), FoundActors);
+	for (AActor* Actor : FoundActors) {
+		AStaticMeshActor* StaticMeshActor = Cast<AStaticMeshActor>(Actor);
+		if (StaticMeshActor != nullptr) {
+			FString Text = StaticMeshActor->GetName();
+		}
+		if (StaticMeshActor != nullptr && StaticMeshActor->GetName() == "StaticMeshActor_6") {
+			this->RotatingObject = StaticMeshActor;
+			break;
+		}
+	}
+}
+
 // Called to bind functionality to input
 void AGodHand::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
-		EnhancedInputComponent->BindAction(this->MoveWorldAction, ETriggerEvent::Triggered,  this, &AGodHand::MoveWorld);
+		EnhancedInputComponent->BindAction(this->MoveWorldAction, ETriggerEvent::Triggered, this, &AGodHand::MoveWorld);
 	}
-	
 }
 
 void AGodHand::MoveWorld(const FInputActionValue& Value) {
-	const FVector2d CurrentValue = Value.Get<FVector2d>();
-	UE_LOG(LogTemp, Log, TEXT("Moving FUnction with the following Paramters: %s"), *CurrentValue.ToString());
-}
+	// TODO: Es gibt noch ein Bug bei der Bewegung da der Yaw Winkel sich aendert selbst wenn nur z.B. der Pitch Winkel veraendert werden soll. 
 
+	
+	if (!this->RotatingObject) {
+		return;
+	}
+	
+	const FVector2d CurrentValue = Value.Get<FVector2d>();
+	FRotator NewRotation = this->RotatingObject->GetActorRotation();
+
+	
+	
+		
+	const float Factor = CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed;
+	float RollFactor = 0;
+	float PitchFactor = 0;
+	float ScaleOfRotation = 0.0;
+
+	// Beide Roll und Pitch muessen Positive sein
+	if (NewRotation.Yaw >= 0 && NewRotation.Yaw < 90) {
+		ScaleOfRotation = NewRotation.Yaw / 90.0;
+
+		RollFactor = Factor * (ScaleOfRotation);
+		PitchFactor = Factor * (1 - ScaleOfRotation);
+		
+		//NewRotation.Roll += CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (ScaleOfRotation);
+		//NewRotation.Pitch += CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (1 - ScaleOfRotation);
+	}
+
+	// Roll muss Positive und Pitch muss NEgative drehen
+	if (NewRotation.Yaw >= 90 && NewRotation.Yaw < 180) {
+		ScaleOfRotation = (NewRotation.Yaw - 90.0) / 90.0;
+
+		RollFactor  = Factor * (1 - ScaleOfRotation);
+		PitchFactor = Factor * (ScaleOfRotation);
+		
+		//NewRotation.Roll += CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (1 - ScaleOfRotation);
+		//NewRotation.Pitch -= CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (ScaleOfRotation);
+	}
+
+	// Roll und PItch mussen NEgative Drehen
+	if (NewRotation.Yaw >= -180 && NewRotation.Yaw < -90) {
+		ScaleOfRotation = (NewRotation.Yaw * -1 - 90.0) / 90.0;
+
+		RollFactor  = Factor *  (1 - ScaleOfRotation);
+		PitchFactor = Factor *  (ScaleOfRotation);
+
+		//NewRotation.Roll -= CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (1 -ScaleOfRotation);
+		//NewRotation.Pitch -= CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (ScaleOfRotation);
+	}
+
+	// Roll muss NEgative und Pitch muss Posisive Gedreht werden
+	if (NewRotation.Yaw >= -90 && NewRotation.Yaw < 0) {
+		ScaleOfRotation = (NewRotation.Yaw * -1) / 90.0;
+
+		RollFactor  = Factor *  (ScaleOfRotation);
+		PitchFactor = Factor *  (1 - ScaleOfRotation);
+		
+		//NewRotation.Roll -= CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (ScaleOfRotation);
+		//NewRotation.Pitch += CurrentValue.Y * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed * (1 -ScaleOfRotation);
+	}
+	
+
+	NewRotation.Roll += RollFactor;
+	NewRotation.Pitch += PitchFactor;
+
+	
+	NewRotation.Yaw += CurrentValue.X * GetWorld()->GetDeltaSeconds() * this->RotationObjectSpeed;
+	this->RotatingObject->SetActorRotation(NewRotation);
+}
--- Source/Transcendet_Life/Characters/GodHand.h
+++ Source/Transcendet_Life/Characters/GodHand.h
@@ -6,53 +6,96 @@
 #include "GameFramework/Pawn.h"
 #include "GodHand.generated.h"
 
+
+class URectLightComponent;
+class AStaticMeshActor;
 struct FInputActionValue;
 class UInputAction;
 class UInputMappingContext;
-class UCameraComponent;
+
+
 
 UCLASS()
 class TRANSCENDET_LIFE_API AGodHand : public APawn
 {
 	GENERATED_BODY()
 
+protected:
+	// Adding the Mapping Context
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* MappingContext;
+
+	/**
+	 * @brief Move Action for the world 
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveWorldAction;
+
+	/**
+	 * @brief The Pointer for the Object what will be Rotated
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Rotation")
+	AStaticMeshActor* RotatingObject;
+
+	/**
+	 * @brief The Rotation Speed for the world
+	 */
+	float RotationObjectSpeed = 50.0f;
+
+private:
+	/** Camera boom positioning the camera behind the character */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class USpringArmComponent* SpringArm;
+
+	/** Follow camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	class UCameraComponent* PlayerCamera;
+	
 public:
 	// Sets default values for this pawn's properties
 	AGodHand();
 
 protected:
+	/** Called for the MoveWorld Input */
+	void MoveWorld(const FInputActionValue& Value);
+
+protected:
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
+
+	
 
 public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-protected:
-	// Adding the Mapping Context
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	UInputMappingContext* MappingContext;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly)
-	UInputAction* MoveWorldAction;
-
-	void MoveWorld(const FInputActionValue& Value);
+	/**
+	 * @brief The Root for the Character
+	 */
+	UPROPERTY(EditDefaultsOnly,BlueprintReadWrite)
+	USceneComponent* Root;
 	
+	/**
+	 * @brief Mesh of the GodHand being dispayed
+	 */
+	UPROPERTY(EditAnywhere)
+	USkeletalMeshComponent* PlayerMesh;
+
+	/**
+	 * @brief The Light over the PLayer to see something
+	 */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "RectLight")
+	URectLightComponent* RectLightComponent;
+
+
 private:
 	/**
-	 * @brief Mesh of the player being dispayed
+	 * @brief this function is called in the BeginPLay to get The Low Poly World to rotate later.
 	 */
-	UPROPERTY(EditDefaultsOnly)
-	UStaticMeshComponent* PlayerMesh;
+	void GetRotaingWorldFormAllActors();
 
-	
-	
-	/**
-	 * @brief Camera to view the scene
-	 */
-	UPROPERTY(EditDefaultsOnly)
-	UCameraComponent* PlayerCamera;
+
 };
