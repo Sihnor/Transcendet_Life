5d06ede95a6bdf8f63c37678b3109328466dce6d
Sihnor <m.rudolph.programming@outlook.com>
2023-06-19 22:50:44 +0200

    Added the Lumberjack Class and added the assets

---
+++ Source/Transcendet_Life/Characters/Professions/Lumberjack/Lumberjack.cpp
@@ -0,0 +1,106 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Lumberjack.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+
+
+// Sets default values
+ALumberjack::ALumberjack() {
+  // Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
+
+  // Character doesnt have a axe at start
+  this->bHasAxe = false;
+
+  // Create a CameraComponent	
+  this->FirstPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+  this->FirstPersonCameraComponent->SetupAttachment(GetCapsuleComponent());
+  this->FirstPersonCameraComponent->SetRelativeLocation(FVector(-10.f, 0.f, 60.f)); // Position the camera
+  this->FirstPersonCameraComponent->bUsePawnControlRotation = true;
+
+  // Create a mesh component that will be used when being viewed from a '1st person' view (when controlling this pawn)
+  Mesh1P = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("CharacterMesh1P"));
+  Mesh1P->SetOnlyOwnerSee(true);
+  Mesh1P->SetupAttachment(FirstPersonCameraComponent);
+  Mesh1P->bCastDynamicShadow = false;
+  Mesh1P->CastShadow = false;
+  //Mesh1P->SetRelativeRotation(FRotator(0.9f, -19.19f, 5.2f));
+  Mesh1P->SetRelativeLocation(FVector(-30.f, 0.f, -150.f));
+  
+  //// Setup for the right position and rotation for the right pose
+  //this->GetMesh()->SetRelativeRotation(FRotator(0.0, -90.0, 0.0));
+  //this->GetMesh()->SetRelativeLocation(FVector(0.0, 0.0, 70.0));
+  //this->SetActorScale3D(FVector(0.1));
+
+  AutoPossessPlayer = EAutoReceiveInput::Player0;
+}
+
+// Called when the game starts or when spawned
+void ALumberjack::BeginPlay() {
+  Super::BeginPlay();
+
+  // Setting up Enhanced Player Input
+  if (const APlayerController* PlayerController = Cast<APlayerController>(this->GetController())) {
+    if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) {
+      Subsystem->AddMappingContext(this->DefaultMappingContext, 0);
+    }
+  }
+}
+
+// Called every frame
+void ALumberjack::Tick(float DeltaTime) {
+  Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void ALumberjack::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+  Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+  if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent)) {
+    // Jumping
+    EnhancedInputComponent->BindAction(this->JumpCharacterAction, ETriggerEvent::Triggered, this, &ACharacter::Jump);
+    EnhancedInputComponent->BindAction(this->JumpCharacterAction, ETriggerEvent::Triggered, this, &ACharacter::StopJumping);
+    
+    // Look
+    EnhancedInputComponent->BindAction(this->LookCharacterAction, ETriggerEvent::Triggered, this, &ALumberjack::Look);
+    
+    // Move
+    EnhancedInputComponent->BindAction(this->MoveCharacterAction, ETriggerEvent::Triggered, this, &ALumberjack::Move);
+  }
+}
+
+
+void ALumberjack::SetHasAxe(const bool bNewHasAxe) {
+  this->bHasAxe = bNewHasAxe;
+}
+
+bool ALumberjack::GetHasAxe() {
+  return this->bHasAxe;
+}
+
+void ALumberjack::Look(const FInputActionValue& Value) {
+  // input is a Vector2D
+  const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+  if (this->GetController() != nullptr) {
+    // add yaw and pitch input to controller
+    this->AddControllerYawInput(LookAxisVector.X);
+    this->AddControllerPitchInput(LookAxisVector.Y);
+  }
+}
+
+void ALumberjack::Move(const FInputActionValue& Value) {
+  // input is a Vector2D
+  const FVector2D MovementVector = Value.Get<FVector2D>();
+
+  if (this->GetController() != nullptr) {
+    // add movement 
+    this->AddMovementInput(this->GetActorForwardVector(), MovementVector.Y);
+    this->AddMovementInput(this->GetActorRightVector(), MovementVector.X);
+  }
+}
---
+++ Source/Transcendet_Life/Characters/Professions/Lumberjack/Lumberjack.h
@@ -0,0 +1,77 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Lumberjack.generated.h"
+
+
+UCLASS()
+class TRANSCENDET_LIFE_API ALumberjack : public ACharacter {
+  GENERATED_BODY()
+
+  //////////////////////////////////////////////////////////////////////////// Input
+  
+  // Adding the Mapping Context
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+  class UInputMappingContext* DefaultMappingContext;
+
+  /** Move Action for the world */  
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+  class UInputAction* MoveCharacterAction;
+
+  /** Jump Action for the world */  
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+  class UInputAction* JumpCharacterAction;
+
+  /** Look Input Action */
+  UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+  class UInputAction* LookCharacterAction;
+
+  //////////////////////////////////////////////////////////////////////////// Components
+
+  /** Pawn mesh: 1st person view (arms; seen only by self) */
+  UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+  class USkeletalMeshComponent* Mesh1P;
+  
+  /** First person camera */
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+  class UCameraComponent* FirstPersonCameraComponent;
+
+public:
+  // Sets default values for this character's properties
+  ALumberjack();
+
+protected:
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
+
+private:
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
+  
+public:
+  /** Bool for AnimBP to switch to another animation set */
+  UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Weapon)
+  bool bHasAxe;
+
+  /** Setter to set the bool */
+  UFUNCTION(BlueprintCallable, Category = Weapon)
+  void SetHasAxe(bool bNewHasAxe);
+
+  /** Getter for the bool */
+  UFUNCTION(BlueprintCallable, Category = Weapon)
+  bool GetHasAxe();
+
+protected:
+  /** Called for the Move Input */
+  void Move(const struct FInputActionValue& Value);
+
+  /** Called for the Look Input */
+  void Look(const struct FInputActionValue& Value);
+
+  // Called to bind functionality to input
+  virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
