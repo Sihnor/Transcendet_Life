030026679c57867373d95a3e9325ffef28ad224b
Sihnor <m.rudolph.programming@outlook.com>
2023-10-02 17:46:34 +0200

    nur ein update

---
+++ Source/Transcendet_Life/Resources/Tree.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Tree.h"
+
+#include "NavigationSystemTypes.h"
+
+
+// Sets default values
+ATree::ATree() {
+  // Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+  PrimaryActorTick.bCanEverTick = true;
+
+  this->Root = CreateDefaultSubobject<USceneComponent>(TEXT("RootCompoennt"));
+  this->SetRootComponent(this->Root);
+
+  this->TreeMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TreeMeshComponent"));
+  this->TreeMesh->SetupAttachment(this->Root);
+}
+
+// Called when the game starts or when spawned
+void ATree::BeginPlay() {
+  Super::BeginPlay();
+  
+}
+
+// Called every frame
+void ATree::Tick(float DeltaTime) {
+  Super::Tick(DeltaTime);
+}
+
---
+++ Source/Transcendet_Life/Resources/Tree.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Tree.generated.h"
+
+UCLASS()
+class TRANSCENDET_LIFE_API ATree : public AActor {
+  GENERATED_BODY()
+
+private:
+  // Root Component
+  UPROPERTY(BlueprintReadOnly, EditAnywhere, meta=(AllowPrivateAccess="true"))
+  USceneComponent* Root;
+  
+  // Mesh of the GodHand being dispayed
+  UPROPERTY(BlueprintReadOnly, EditAnywhere, meta = (AllowPrivateAccess = "true"))
+  UStaticMeshComponent* TreeMesh;
+
+public:
+  // Sets default values for this actor's properties
+  ATree();
+
+protected:
+  // Called when the game starts or when spawned
+  virtual void BeginPlay() override;
+
+public:
+  // Called every frame
+  virtual void Tick(float DeltaTime) override;
+};
